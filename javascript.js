89acb1d9c0a1e0a8539ef53172bf34beb59e834c
realMNohgee <clund25@gmail.com>
2022-03-31 14:28:59 -0500

    still working out the functions of the calculator. Just finished  the compute() function and prior to that I finished up the chooseOperation function

--- javascript.js
+++ javascript.js
@@ -4,9 +4,76 @@ const equalsButton = document.querySelector('[data-equals]');
 const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
-const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
-            // Create a way to store the output //
+// Hook all variables and make operate on the calculator object //
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+            // Create and define the operations of the calculator //
+
+function clear() {
+    this.currentOperand = '';
+    this.previousOperand = '';
+    this.operation = undefined;
+}
+
+ function delete() {}
+
+percentage(numbers, percent) {
+    return (numbers/100) * percent;
+}
+
+// Making sure displayed values are constantly updated every time we click a button on the calculator //
+
+appendNumber(number){
+    if(number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+}
+
+ // Making the operation buttons (+ - * etc) work on the calculator //
+
+ chooseOperation(operation) {
+    if (this.currentOperand === '') return 
+    if (this.previousOperand !== '') {
+        this.compute()
+    }
+    this.operation = operation
+    this.previousOperand = this.currentOperand
+    this.currentOperand = ''
+};
+
+             // Programming the '=' button //
+
+compute() {
+    let computation
+    const prev = parseFloat(this.previousOperand)
+    const current = parseFloat(this.currentOperand)
+    if (isNaN(prev) || isNaN(current)) return
+    switch (this.operation) {
+        case '+' :
+            computation = prev + current
+            break
+        case '-' :
+            computation = prev - current 
+            break
+        case '*' :
+            computation = prev * current
+            break
+        case '÷' :
+            computation = prev / current
+            break
+        default:
+            return               
+    }
+    this.currentOperand = computation
+    this.operation = undefined
+    this.previousOperand = ''
+}
+
+updatedDisplay() {
+
+}
+
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
@@ -14,48 +81,24 @@ class Calculator {
         this.clear()
     }
 }
-            // Create and define the operations of the calculator //
 
-clear() {
-    this.currentOperand = ''
-    this.previousOperand = ''
-    this.operation = undefined
-}
-
-delete() {
-
-}
-
-percentage(numbers, percent) {
-    return (numbers/100) * percent;
-}
-
-appendNumber(number) {
-    if(number === '.' && this.currentOperand.includes('.')) return
-    this.currentOperand = this.currentOperand.toString() + number.toString()
-}
-
-chooseOperation(operation) {
-
-};
-
-compute() {
-
-};
-
-updatedDisplay() {
-
-}
-
-const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
-
-// Making sure displayed values are constantly updated every time we click a button on the calculator //
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updatedDisplay()
+    })
+})
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () => {
         calculator.appendNumber(button.innerText)
     })
 })
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updatedDisplay()
+})   
 
 
 
