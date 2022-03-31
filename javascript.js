89811d281e282db3e3037ebe1a08d168380d4dba
realMNohgee <clund25@gmail.com>
2022-03-31 13:44:45 -0500

    added the functions for appendNumber, clear, and percentage.  I also added an eventListener('click') for the numbers buttons.

--- javascript.js
+++ javascript.js
@@ -6,3 +6,56 @@ const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
+            // Create a way to store the output //
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
+}
+            // Create and define the operations of the calculator //
+
+clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+}
+
+delete() {
+
+}
+
+percentage(numbers, percent) {
+    return (numbers/100) * percent;
+}
+
+appendNumber(number) {
+    if(number === '.' && this.currentOperand.includes('.')) return
+    this.currentOperand = this.currentOperand.toString() + number.toString()
+}
+
+chooseOperation(operation) {
+
+};
+
+compute() {
+
+};
+
+updatedDisplay() {
+
+}
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+// Making sure displayed values are constantly updated every time we click a button on the calculator //
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+    })
+})
+
+
+
